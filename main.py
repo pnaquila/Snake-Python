da88c3aa7b263fa1dcb29064986bdea5058b69aa
aonanam <aonanam@bu.edu>
2013-12-05 10:45:01 -0500

    Test

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 
 def collide(x1, x2, y1, y2, w1, w2, h1, h2):
-	if x1+w1>x2 and x1<x2+w2 and y1+h1>y2 and y1<y2+h2:
+	if x1+w1>x2 and x1<x2+w2 and y1+h1>y2 and y1<y2+h2: #Shane wuz heer!
 		return True
 	else:
 		return False
